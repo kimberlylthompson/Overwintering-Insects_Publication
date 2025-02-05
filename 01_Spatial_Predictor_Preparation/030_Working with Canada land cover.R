265b2c9f3866622eb18cfe60a87e5c9e65a1e8ad
kimberlylthompson <kthompson25@wisc.edu>
2025-02-05 15:32:44 +0100

    data prep

--- 01_Spatial_Predictor_Preparation/030_Working with Canada land cover.R
+++ 01_Spatial_Predictor_Preparation/030_Working with Canada land cover.R
@@ -8,16 +8,18 @@
 # This code reclassifies land cover into 6
 # classes and crops to the Great Lakes Region
 
-# Done for both USA and Canada but this is for USA
+# Done for both USA and Canada - but this is for Canada
 
 # USA: data from USGS (Multi-resolution Land characteristics Consortium)
 # downloaded for the year 2016
 # from https://www.mrlc.gov/data?f%5B0%5D=category%3ALand%20Cover
 
-# Canada: 
+# Canada: data from Canadian Governemnt - 2015 land cover
+# https://open.canada.ca/data/en/dataset/4e615eae-b90c-420b-adee-2ca35896caf6/resource/54200576-6203-4c1f-b646-924a1df3e08b
 
 # Produces dataset:
 #'US Great Lakes 2016 Landcover.tif'
+#'Canada Great Lakes 2015 Landcover.tif'
 
 
 # clean workspace and load required packages #
@@ -40,10 +42,10 @@ setwd("00_Data")
 # region <- sf :: st_read("~/share/groups/mas/04_personal/Kim_T/Data/Great_Lakes_States and Prov.shp")
 region <- sf :: st_read("Great_Lakes_States and Prov.shp")
 
-# Load the USA 2016 land cover file
+# Load the Canada 2015 land cover file
 # (Not stored in 00_Data of Github repo)
-nlcd <- terra :: rast("D:/My Drive/Ch 4 Bumblebees/00_Data/Raw/NLCD 2016/Annual_NLCD_LndCov_2016_CU_C1V0.tif")
-# nlcd <- terra :: rast("~/share/groups/mas/04_personal/Kim_T/Insect Cold Tolerance/00_Data/Raw/NLCD 2016/Annual_NLCD_LndCov_2016_CU_C1V0.tif")
+nlcd <- terra :: rast("H:/My Drive/Ch 4 Bumblebees/00_Data/Canadian_Data")
+# nlcd <- terra :: rast("~/share/groups/mas/04_personal/Kim_T/Insect Cold Tolerance/00_Data/Raw/Canadian land cover 2015/landcover-2015-classification.tif")
 
 
 ###################################
@@ -52,9 +54,9 @@ nlcd <- terra :: rast("D:/My Drive/Ch 4 Bumblebees/00_Data/Raw/NLCD 2016/Annual_
 ###                             ###
 ###################################
 
-############
-##  USA   ##
-############
+###############
+##  Canada   ##
+###############
 
 # Convert region so that it match nlcd:
 region2 <- sf :: st_transform( region, crs(nlcd) )
@@ -65,38 +67,39 @@ nlcd_crop <- terra :: crop(nlcd, region2)
 plot(region2$geometry)
 plot(nlcd_crop, add=TRUE)
 
-# Collapse some of the landcover classes (using the big NLCD raster)
+# Collapse some of the landcover classes 
 # Generate landcover number to name conversions:
-num.codes <- unique( unlist( nlcd ) ) # This step takes a while
 num.codes2 <- unique( unlist( nlcd_crop ) ) # Run this too just to see which classes are present in the cropped
 
 # Add names for each category based on metadata
-num.codes2$cover.names <- c("Open Water", "Developed Open Space",
-                            "Developed Low Intensity", "Developed Medium Intensity",
-                            "Developed High Intensity", "Barren Land",
-                            "Deciduous Forest", "Evergreen Forest",
-                            "Mixed Forest", "Shrub Scrub", "Grassland Herbaceous",
-                            "Pasture Hay", "Cultivated Crops", "Woody Wetlands",
-                            "Emergent Herbaceous Wetlands")
-
-# Add row for no data
-num.codes2[16, ] <- c("250", "No Data")
+num.codes2$cover.names <- c("Temperate or sub-polar needleleaf forest",
+                            "Sub-polar taiga needleleaf forest",
+                            "Temperate or sub-polar broadleaf deciduous forest",
+                            "Mixed forest", "Temperate or sub-polar shrubland",
+                            "Temperate or sub-polar grassland",
+                            "Sub-polar or polar shrubland-lichen-moss",
+                            "Sub-polar or polar grassland-lichen-moss",
+                            "Sub-polar or polar barren-lichen-moss",
+                            "Wetland", "Cropland", "Barren lands",
+                            "Urban", "Water", "Snow and Ice")
 
 
 # Reclassify nlcd_crop into 6 classes:
-# "1", Deciduous: Deciduous forest (41), Mixed forest (43), Woody wetlands (90)
-# "2", Conifer: Evergreen forest (42), Scrub/shrub (52)
-# "3", Open: Developed, Open Space (21), Barren Land (31), Grassland/Herbaceous (71), Pasture/Hay (81),
-#             Cultivated Crops (82), Emergent Herbaceous Wetlands (95)
-# "4", Urban: Developed, Low Intensity (22), Developed, Medium Intensity (23), Developed High Intensity (24)
-# "5", Water: Open Water (11)
-# "6", Unclassified: Unclassified (250)
+# "1", Deciduous: Temperate or subpolar deciduous (3), Mixed forest (5)
+# "2", Conifer: Temperate or subpolar needleleaf (1), sub-polar or taiga needleleaf (2),
+#               Temperate or subpolar shrubland (6)
+# "3", Open: Temperate or subpolar grassland(8), subpolar or polar shrubland lichen moss (10),
+#           subpolar or polar grassland lichen moss(11), subpolar or polar barren lichen moss (12),
+#            Wetland (13), cropland (14), Barren lands (15)
+# "4", Urban: Urban (16)
+# "5", Water: Water (17)
+# "6", Snow and Ice (18)
 
 
 # Make a reclassification matrix
 # from - each existing class, to - what it should turn into
-rc <- as.matrix(data.frame(from=c(41,43,90,42,52,21,31,71,81,82,95,22,23,24,11,250),
-                           to=c(1,1,1,2,2,3,3,3,3,3,3,4,4,4,5,6)))
+rc <- as.matrix(data.frame(from=c(3,5,1,2,6,8,10,11,12,13,14,15,16,17,18),
+                           to=c(1,1,2,2,2,3,3,3,3,3,3,3,4,5,6)))
 
 # Reclassify
 nlcd_crop <- terra :: classify(nlcd_crop, rc, include.lowest = TRUE)
@@ -104,6 +107,7 @@ nlcd_crop <- terra :: classify(nlcd_crop, rc, include.lowest = TRUE)
 
 # Specify colors so that plot looks alright
 breakpoints <- c(0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5)
+breakpoints <- c(1.5, 2.5, 3.5, 4.5, 5.5, 6.5)
 colors <- c("darkgoldenrod3", "darkgreen", "khaki2", "deeppink", "blue", "black")
 
 # Plot reclassfied raster
@@ -111,25 +115,14 @@ plot(nlcd_crop, breaks=breakpoints, col=colors)
 plot(region2$geometry, add=TRUE)
 
 
-# Make the raster categorical
-cat_df <- data.frame(
-  id = 1:6,
-  category = c("Deciduous", "Conifer", "Open", "Urban", "Open Water", "Unclassified")
-)
-
-
-# Set the levels of the raster
-levels(nlcd_crop) <- cat_df
-
 # Make sure the raster is treated as categorical
 nlcd_crop <- as.factor(nlcd_crop)
 
+# Set the levels with category names
+levels(nlcd_crop) <- data.frame(ID = 1:6, category = c("Deciduous", "Conifer", "Open", "Urban", "Open Water",
+                                                       "Snow Ice"))
+
 # Save the reclassified and cropped NLCD raster
 path <- "~/share/groups/mas/04_personal/Kim_T/Insect Cold Tolerance/00_Data/Spatial Predictors/Individual Land cover/"
-path <- getwd()
-writeRaster(nlcd_crop, paste(path, "US Great Lakes 2016 Landcover.tif", sep == ""))
-
-
-
-
-
+# path <- getwd()
+writeRaster(nlcd_crop, paste(path, "Canada Great Lakes 2016 Landcover.tif", sep = ""))
